059ce644e0399ab6b737613d3b93db240a0cecae
Yuliana Staykova <juleto7@abv.bg>
2016-11-18 13:57:20 +0200

    Create binTree.cpp

---
+++ BinaryTreeSearch/binTree.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+//ne se pishe using namespace std;
+
+struct Node {
+int data;
+Node* left;
+Node* right;
+Node(int data, Node* left = nullptr, Node* right = nullptr ) :
+ data(data), left (left), right(right) {}
+ 
+ class BST {
+ int rootData;
+ BST* lweftTree;
+ BST* rightTree;
+ 
+ public: 
+// BST() : root(nullptr) {}
+ //bool empty() {
+ //return root ++ nullptr;
+ //}
+ 
+ void add(int x)
+ {
+ 
+ }
+ };
+ 
+ int main() {
+ 
+ 
+ system("pause");
+ return 0;
+ }
+ 
