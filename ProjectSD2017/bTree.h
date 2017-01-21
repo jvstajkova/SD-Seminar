976694802861976054689352911af049818198ca
Yuliana Staykova <juleto7@abv.bg>
2017-01-21 23:21:36 +0200

    Update bTree.h

--- ProjectSD2017/bTree.h
+++ ProjectSD2017/bTree.h
@@ -1,44 +1,42 @@
 #pragma once
-#ifndef BTREE_H_
-#define BTREE_H_
+#ifndef B_TREE
+#define B_TREE
 
-#include<iostream>
-#include<vector>
-#include<fstream>
+#include <vector>
 #include "student.h"
-
-using std::ofstream;
+#include <fstream>
 using std::vector;
+using std::ofstream;
+struct BTreeNode
+{
+	vector<Student> allStudents;
+	int minimumDegree;
+	vector<BTreeNode*> children;
+	BTreeNode(int);
 
-struct bTreeNode {
-	vector<Student> students;
-	int minDegree; //minimum degree = minimum number of keys
-	vector<bTreeNode*> children;
-	bTreeNode(int);
+
+
 };
 
-class bTree {
-private:
-	bTreeNode *root;
-	int minDegree;
-	void splitter(bTreeNode*, int, bTreeNode*);//splits the given child node at the child node's given index
-	void insertInTree(bTreeNode*, Student&);
-	//void remove(bTreeNode*, Student&);
-	void printInFile(bTreeNode*, ostream&);
-	Student* searchFor(bTreeNode*, const char*); //search by name
+class BTree
+{
+	BTreeNode *root;
+	int minimumDegree;
+	void splitHelper(BTreeNode*, int, BTreeNode*);
+	void insertHelper(BTreeNode*, Student&);
+	void printHelper(BTreeNode*, ostream&);
+	Student* searchHelper(BTreeNode*, const char*);
 public:
-	int amount();
-	bTree(int);
-	vector<Student> getRoot();
-	void insertNewRecords(Student);//inserts new records into the node
+	int capacity();
+	BTree(int);
+	void add(Student);
 	bool isEmpty();
-	void printRecords();
-	void printBTree(); //prints the bTree into file
-	void searchForPhoneNumber(const char* name);//finds student's phone number by student's name
-	void searchForGrade(const char* name); // finds student's grade by his name
-	void updateGrade(const char* name);//updates student's grade
-	void updatePhoneNumber(const char* name); //updates student's phone number
-
+	void print();
+	vector<Student> getRoot();
+	void printBTreeInFile();
+	void findNumber(const char* name);
+	void findAverage(const char* name);
+	void changeAverage(const char* name);
+	void changeTelephoneNumber(const char* name);
 };
-
-#endif BTREE_H
+#endif B_TREE
