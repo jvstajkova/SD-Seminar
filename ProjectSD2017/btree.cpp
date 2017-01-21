383ef0411a46022b9c48677ab3e4a37285cb43e8
Yuliana Staykova <juleto7@abv.bg>
2017-01-21 23:27:49 +0200

    Update btree.cpp

--- ProjectSD2017/btree.cpp
+++ ProjectSD2017/btree.cpp
@@ -1,211 +1,256 @@
-#include<iostream>
-#include "bTree.h"
+#include "BTree.h"
+#include <iostream>
 
 using std::cout;
 using std::cin;
-using std::endl;
 
-bTreeNode::bTreeNode(int _min) : minDegree(_min) {
-	students.reserve(2 * minDegree - 1);
-	children.reserve(2 * minDegree);
+BTreeNode::BTreeNode(int _minimum) : minimumDegree(_minimum)
+{
+	allStudents.reserve(2 * minimumDegree - 1);
+	children.reserve(2 * minimumDegree);
 }
 
-bTree::bTree(int _minD) : minDegree(_minD) {
+BTree::BTree(int _minimumDegree) : minimumDegree(_minimumDegree)
+{
 	root = nullptr;
 }
 
-int bTree::amount() {
-	return root->students.size();
+int BTree::capacity()
+{
+	return root->allStudents.size();
 }
-void bTree::splitter(bTreeNode* root, int index, bTreeNode* previousNode) { //make sure thet a node has space for key before the key is inserted 
-																			//splits the child from the node
-																			// root=new node, index = NULL, previousNode = root
-	bTreeNode *newNode = new bTreeNode(previousNode->minDegree); // the new node is initialized as root
 
-	for (int i = 0; i < root->minDegree - 1; i++) { //copy the last (minimum degree - 1) keys of newNode to its child
-		newNode->students.push_back(previousNode->students[i + root->minDegree]);
+void BTree::splitHelper(BTreeNode* root, int index, BTreeNode* oldNode) //root = newNode, index =0 , oldNode=root
+{
+	BTreeNode *newNode = new BTreeNode(oldNode->minimumDegree);
+	for (int i = 0; i < root->minimumDegree - 1; i++)
+	{
+		newNode->allStudents.push_back(oldNode->allStudents[i + root->minimumDegree]);
 	}
 
-	if (previousNode->children.size() != 0) { //copy the last minimum degree keys of newNode to its child
-		for (int i = 0; i < root->minDegree; i++) {
-			newNode->children.push_back(previousNode->children[i + root->minDegree]);
+	if (oldNode->children.size() != 0)
+	{
+		for (int i = 0; i < root->minimumDegree; i++)
+		{
+			newNode->children.push_back(oldNode->children[i + root->minimumDegree]);
 		}
 	}
 
-	previousNode->students.pop_back(); //reduce the number of keys in newNode
-	for (int i = root->students.size(); i >= index + 1; i--) { //create space of new child since the node is going to have one
+	oldNode->allStudents.pop_back();
+	for (int i = root->allStudents.size(); i >= index + 1; i--)
+	{
 		root->children.insert(root->children.begin() + (i + 1), root->children[i]);
 		root->children.erase(root->children.begin() + (i));
 	}
 
-	root->children.insert(root->children.begin() + (index + 1), newNode); //link the new child to this node
-	for (int i = root->students.size() - 1; i >= index; i--) {
-		root->students.insert(root->students.begin() + (i + 1), root->students[i]);
-		root->students.erase(root->students.begin() + (i));
+	root->children.insert(root->children.begin() + (index + 1), newNode);
+
+	for (int i = root->allStudents.size() - 1; i >= index; i--)
+	{
+		root->allStudents.insert(root->allStudents.begin() + (i + 1), root->allStudents[i]);
+		root->allStudents.erase(root->allStudents.begin() + (i));
 	}
 
-	root->students.insert(root->students.begin() + (index), previousNode->students[root->minDegree - 1]);
-	for (int i = previousNode->students.size(); i >= root->minDegree; i--) {
-		previousNode->students.pop_back();
+	root->allStudents.insert(root->allStudents.begin() + (index), oldNode->allStudents[root->minimumDegree - 1]);
+	for (int i = oldNode->allStudents.size(); i >= root->minimumDegree; i--)
+	{
+		oldNode->allStudents.pop_back();
+
+	}
+
+}
+
+void BTree::insertHelper(BTreeNode* root, Student &s)
+{
+	int index = root->allStudents.size() - 1;
+
+	if (root->children.size() == 0)
+	{
+		while (index >= 0 && compareNames(s.getName(), root->allStudents[index].getName()) < 0)
+		{
+			root->allStudents.insert(root->allStudents.begin() + (index + 1), root->allStudents[index]);
+			root->allStudents.erase(root->allStudents.begin() + (index));
+
+			index--;
+
+		}
+		root->allStudents.insert(root->allStudents.begin() + (index + 1), s);
+	}
+	else
+	{
+		while (index >= 0 && compareNames(s.getName(), root->allStudents[index].getName()) < 0)
+		{
+			index--;
+		}
+
+		if (root->children[index + 1]->allStudents.size() == (2 * root->minimumDegree) - 1)
+		{
+			splitHelper(root, index + 1, root->children[index + 1]);
+			if (compareNames(s.getName(), root->allStudents[index + 1].getName()) >0)index++;
+		}
+
+		insertHelper(root->children[index + 1], s);
+
 	}
 }
 
-void bTree::insertInTree(bTreeNode* root, Student &student) {
-	int index = root->students.size() - 1; //initialize index as index of rightmost element
-
-	if (root->children.size() == 0) { //if this is a leaf node
-		while (index >= 0 && compareStudentsNames(student.getName(), root->students[index].getName()) < 0) {
-			//finds the location of new key to be inserted
-			//moves all greater keys to one place ahead
-			root->students.insert(root->students.begin() + (index + 1), root->students[index]);
-			root->students.erase(root->students.begin() + (index));
-			index--;
-		}
-		//insert the new key at found location
-		root->students.insert(root->students.begin() + (index + 1), student);
-	}
-	else {//if this node is not leaf
-		while (index >= 0 && compareStudentsNames(student.getName(), root->students[index].getName()) < 0) {
-			//find the child which is goint to have the new key
-			index--;
-		}//checks if the found child is full
-		if (root->children[index + 1]->students.size() == (2 * root->minDegree) - 1) {
-			//splits the child if it is full
-			splitter(root, index + 1, root->children[index + 1]);
-			//after the split, the middle key goes up
-			//checks which of the two is going to have the new key
-			if (compareStudentsNames(student.getName(), root->students[index + 1].getName()) > 0)
-				index++;
-		}
-		insertInTree(root->children[index + 1], student);
-	}
-}
-
-void bTree::printInFile(bTreeNode* root, ostream& out) {
+void BTree::printHelper(BTreeNode* root, ostream& o)
+{
 	unsigned index = 0;
-	for (index = 0; index < root->students.size(); index++) {
-		if (root->children.size() != 0) {
-			printInFile(root->children[index], out);
+	for (index = 0; index < root->allStudents.size(); index++)
+	{
+		if (root->children.size() != 0)
+		{
+			printHelper(root->children[index], o);
 		}
-		out << root->students[index] << endl;
+		o << root->allStudents[index] << '\n';
 	}
 
-	if (root->children.size() != 0) {
-		printInFile(root->children[index], out);
+	if (root->children.size() != 0)
+	{
+		printHelper(root->children[index], o);
 	}
-
+	/*if (root->children.size())
+	{
+	for (BTreeNode* b : root->children)
+	{
+	printHelper(b, o);
+	cout << '\n';
+	}
+	}*/
 }
 
-Student* bTree::searchFor(bTreeNode* node, const char* name) {
+Student* BTree::searchHelper(BTreeNode* node, const char* name)
+{
 	int index = 0;
-	while (index < node->students.size() && compareStudentsNames(name, node->students[index].getName()) > 0) {
+	while (index < node->allStudents.size() && compareNames(name, node->allStudents[index].getName())>0)
+	{
 		index++;
 	}
-	if (compareStudentsNames(name, node->students[index].getName()) == 0)
-		return &node->students[index];
-	if (node->children.size() == 0)
-		return &Student();
-	return searchFor(node->children[index], name);
+	if (compareNames(name, node->allStudents[index].getName()) == 0) return &node->allStudents[index];
+	if (node->children.size() == 0) return &Student();
+	return searchHelper(node->children[index], name);
 }
 
-void bTree::printRecords() {
-	if (root != nullptr) {
-		printInFile(root, cout);
+void BTree::print()
+{
+	if (root != nullptr)
+	{
+		printHelper(root, cout);
 	}
-	else cout << "The database is empty!" << endl;
+	else cout << "Empty database!" << '\n';
 }
 
-vector<Student> bTree::getRoot() {
-	return root->students;
+vector<Student> BTree::getRoot()
+{
+	return root->allStudents;
 }
 
-void bTree::printBTree() {
-	ofstream outputFile("students.txt");
-	printInFile(root, outputFile);
+void BTree::printBTreeInFile()
+{
+	ofstream studentsFile("students.txt");
+	printHelper(root, studentsFile);
 
 }
 
-void bTree::searchForPhoneNumber(const char* name) {
-	if (searchFor(root, name) == &Student()) {
-		warningMessage();
+void BTree::findNumber(const char* name)
+{
+	if (searchHelper(root, name) == &Student())
+	{
+		errorMessage();
 	}
-	if (root != nullptr && !(searchFor(root, name) == &Student())) {
-		cout << "The student with name: " << name << " has the following phone number: " << (*searchFor(root, name)).getPhoneNumber() << endl;
+	if (root != nullptr && !(searchHelper(root, name) == &Student()))
+	{
+		cout << "The student " << name << " has number " << (*searchHelper(root, name)).getNumber() << "." << '\n';;
 	}
-	if (root == nullptr) {
-		cout << "The database is empty." << endl;
-	}
-}
-
-void bTree::searchForGrade(const char * name) {
-	if (searchFor(root, name) == &Student()) {
-		warningMessage();
-	}
-
-	if (root != nullptr && !(searchFor(root, name) == &Student())) {
-		cout << "The student with name: " << name << " has the following grade: " << (*searchFor(root, name)).getGrade() << endl;
-	}
-	if (root == nullptr) {
-		cout << "The database is empty." << endl;
+	if (root == nullptr)
+	{
+		cout << "Empty database" << '\n';
 	}
 }
-void bTree::updateGrade(const char* name) {
-	if (isEmpty())
-		warningMessage();
-	if (!(searchFor(root, name) == &Student())) {
 
-		cout << name << " 's previous grade is " << (*searchFor(root, name)).getGrade() << endl;
-		cout << "Insert new grade for this student: " << endl;
+void BTree::findAverage(const char * name)
+{
+	if (searchHelper(root, name) == &Student())
+	{
+		errorMessage();
+	}
 
-		double newGrade;
-		cin >> newGrade;
+	if (root != nullptr && !(searchHelper(root, name) == &Student()))
+	{
+		cout << "The student " << name << " has average " << (*searchHelper(root, name)).getAverage() << "." << '\n';
+	}
+	if (root == nullptr)
+	{
+		cout << "Empty database" << '\n';
+	}
+}
+void BTree::changeAverage(const char* name)
+{
+	if (isEmpty()) errorMessage();
+	if (!(searchHelper(root, name) == &Student())) {
 
-		(*searchFor(root, name)).setGrade(newGrade);
-		cout << "The student's new grade is " << (*searchFor(root, name)).getGrade() << endl;
+		cout << name << " 'old average  " << (*searchHelper(root, name)).getAverage() << '\n';
+		cout << "Enter new average for this student:" << '\n';
+		double newAverage;
+
+		cin >> newAverage;
+
+		(*searchHelper(root, name)).setAverage(newAverage);
+		cout << "The new average - " << (*searchHelper(root, name)).getAverage() << '\n';
+
 
 	}
-	else warningMessage();
+	else errorMessage();
 }
-void bTree::updatePhoneNumber(const char * name) {
-	if (isEmpty())
-		warningMessage();
-	if (!(searchFor(root, name) == &Student())) {
-		cout << name << " 's previous phone number is " << (*searchFor(root, name)).getPhoneNumber() << endl;
-		cout << "Insert new phone number for this student:" << endl;
+void BTree::changeTelephoneNumber(const char * name)
+{
+	if (isEmpty()) errorMessage();
+	if (!(searchHelper(root, name) == &Student())) {
 
-		unsigned long newPhoneNumber;
-		cin >> newPhoneNumber;
-		(*searchFor(root, name)).setPhoneNumber(newPhoneNumber);
-		cout << "The student's new phone number is " << (*searchFor(root, name)).getPhoneNumber() << endl;
+		cout << name << " 'old telephone number  " << (*searchHelper(root, name)).getNumber() << '\n';
+		cout << "Enter new average for this student:" << '\n';
+		double newNumber;
+
+		cin >> newNumber;
+
+		(*searchHelper(root, name)).setNumber(newNumber);
+		cout << "The new average - " << (*searchHelper(root, name)).getNumber() << '\n';
 	}
-	else warningMessage();
+	else errorMessage();
 }
 
-bool bTree::isEmpty() {
+bool BTree::isEmpty()
+{
 	return (root == nullptr);
 }
 
-void bTree::insertNewRecords(Student student) {
-	if (root == nullptr) {
-		root = new bTreeNode(minDegree);
-		root->students.push_back(student);
+void BTree::add(Student s)
+{
+	if (root == nullptr)
+	{
+		root = new BTreeNode(minimumDegree);
+		root->allStudents.push_back(s);
 	}
-	else {
-		if (root->students.size() == (2 * minDegree - 1)) {
-			bTreeNode *newNode = new bTreeNode(minDegree);
+	else
+	{
+		if (root->allStudents.size() == (2 * minimumDegree - 1))
+		{
+			BTreeNode *newNode = new BTreeNode(minimumDegree);
 			newNode->children.push_back(root);
-			splitter(newNode, 0, root);
+			splitHelper(newNode, 0, root);
 			int index = 0;
-			if (compareStudentsNames(student.getName(), newNode->students.front().getName()) > 0) {
+			if (compareNames(s.getName(), newNode->allStudents.front().getName()) > 0)
+			{
 				index++;
 			}
-			insertInTree(newNode->children[index], student);
+			insertHelper(newNode->children[index], s);
 			root = newNode;
 		}
-		else {
-			insertInTree(root, student);
+		else
+		{
+			insertHelper(root, s);
 		}
 
 	}
 }
-
